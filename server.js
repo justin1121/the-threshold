01318a27135ce533132ff93ea7f6531233933796
Justin Patriquin <justinpatriquin@gmail.com>
2013-11-13 20:38:41 +0000

    added some template and docs for future ref

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var express = require('express'),
     redis = require('redis'),
     io = require('socket.io').listen(server),
     fs = require('fs');
-    go = require('./goinstant.js');
 
 var port = 80;
 var host = '';
@@ -44,6 +43,71 @@ app.get('/threshold', function(req, res){
 
 });
 
+/* 
+ * Redis Infrastrucutre:
+ *
+ * Need to create a background API for dealing with the different functions
+ * concerning the below infrastructure. The API will then be able to be
+ * accessed easily from a few different ways. Including: web interface, text
+ * interface.
+ *
+ * (phone #) - (room)
+ *
+ * (room) -> users 
+ *
+ * sms-(room) -> messages
+ *
+ * Anything surronded in '()' are variables to be filled in with user
+ * specified info. '-' characters mean its a key to value pair. '->' 
+ * characters mean key to list value.
+ *
+ * Redis side note:
+ *
+ * Investigate just putting messages into redis and have a pub-sub
+ * system where it forwards the message out to clients when it gets added.
+ * Might scale better? Could have messages coming in from other services 
+ * other then just the current twilio GET request. Might cause delay, but
+ * probably not, just seems a little weird to have two data sources feeding
+ * the app.
+ *
+ * Redis vs. Postgres:
+ *
+ * Might make a neat drop in replacement for redis and use postgres and maybe
+ * eventually do some A/B testing and see which ones scales better. Problem
+ * with this is getting enough traffic where these things could be stressed
+ * in some way or another.
+ *
+ * Subscribing to more than on room?
+ *
+ * Not sure how going to handle this...maybe doesn't make sense seeing as
+ * user receiving the message to their phone would have to parse out the
+ * differences in the single stream. I think maybe this is where text message
+ * meta data being parsed out by an android app would come in handy. Side note:
+ * a twitter bootstrap like thing just came out for android. Anyway kitkat 4.4
+ * comes with SMS apis, so maybe can use this to receive text messages from
+ * certain numbers and parse out the meta data to route messages. Hopefully
+ * these SMS apis will be backported.
+ *
+ */
+
+io.sockets.on('connection', function(socket){
+  socket.on('createRoom', function(data){
+    // check if room exists
+    // check is # is valid in CAN and US and doesn't already exist
+    // create room with name
+    // subscribe to room
+  });
+
+  socket.on('subscribe', function(data){
+    // check is # is valid in CAN and US and make sure it actually exists
+
+  });
+
+  socket.on('unsubscribe', function(data){
+    // check if # actually exists if not emit something 
+  });
+});
+
 app.get('/sms', function(req, res){
   if(req['query']['clear']){
     console.log('Deleting SMS list.');
@@ -78,7 +142,6 @@ var readConfig = function(){
     dbport = cdata['dbport'];
     setLogging(cdata['log']);
     connectRedis();
-    go.connectGoInstant(cdata['go-client-id'], cdata['go-client-secret']);
   });
 };
 
