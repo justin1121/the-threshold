d66b620663c4bff5c120a1e5b0141398333acc81
Justin Patriquin <justinpatriquin@gmail.com>
2013-12-10 17:42:26 +0000

    Cleaned up with linter, changed config processing around removed my site front, redirection and sendMessages function

--- server.js
+++ server.js
@@ -1,39 +1,77 @@
+/* jshint node: true */
+'use strict';
+
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    twilio = require('twilio'),
-    redis = require('redis'),
     io = require('socket.io').listen(server),
     fs = require('fs'),
     dbclient = require('./redis-rooms.js'),
     auth = require('./auth.js');
 
-var port = 80;
+var port;
+var host = '';
 var dbhost = '';
 var dbport = '';
-var authConnString = '';
+var authConnString = null;
 var dbclient;
 
-server.listen(port, function(){
-  readConfig();
+var readConfig = function(cb){
+  fs.readFile('./config.json', function(err, data){
+    if(err){
+      throw err;
+    }
 
-  console.log('Listening on ' + port);
-});
+    var cdata = JSON.parse(data);
+    if(!cdata['host']){
+      throw new Error('Config parameter (host) is required.');
+    }
+    if(!cdata['port']){
+      throw new Error('Config parameter (port) is required.');
+    }
+    if(!cdata['dbhost']){
+      throw new Error('Config parameter (dbhost) is required.');
+    }
+    if(!cdata['dbport']){
+      throw new Error('Config parameter (dbport) is required.');
+    }
+    if(!cdata['authConnString']){
+      throw new Error('Config parameter (authConnString) is required.');
+    }
+
+    host = cdata['host'];
+    port = cdata['port'];
+    dbhost = cdata['dbhost'];
+    dbport = cdata['dbport'];
+    authConnString = cdata['authConnString'];
+
+    setLogging(cdata['log']);
+    connectDb();
+    if(cb){
+      cb();
+    }
+  });
+};
+
+(function(){
+  readConfig(function(){
+    server.listen(port, function(){
+      console.log('Listening on ' + port);
+    });
+  });
+})();
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.bodyParser());
 app.use(express.cookieParser());
-app.use(express.cookieSession({secret: '19920606', 
+app.use(express.cookieSession({secret: '19920606',
                                cookie: {maxAge: 3600000}}));
-
-app.get('/', function(req, res){
-  res.render('circumpunct.ejs');
-});
 
 app.get('/auth', function(req, res){
   var json = {};
   json.err = 0;
+  json.host = host;
 
   if(req.session.auth){
     res.redirect('/admin');
@@ -49,7 +87,8 @@ app.get('/auth', function(req, res){
 });
 
 app.post('/auth', function(req, res){
-  auth.authenticate(req['body']['inputUser'], req['body']['inputPass'], authConnString, function(auth){
+  var user = req['body']['inputUser'], pass = req['body']['inputPass'];
+  auth.authenticate(user, pass, authConnString, function(auth){
     if(auth){
       req.session.auth = 1;
       res.redirect('/' + req.session.source);
@@ -57,10 +96,10 @@ app.post('/auth', function(req, res){
     else{
       var json = {};
       json.err = 1;
+      json.host = host;
       res.render('auth.ejs', json);
     }
-  }); 
-
+  });
 });
 
 app.get('/admin', function(req, res){
@@ -71,17 +110,18 @@ app.get('/admin', function(req, res){
   }
   else{
     dbclient.getAllRooms(function(reply){
-      var json = {};              
+      var json = {};
       json.rooms = reply;
       json.auth = 1;
-      json.err = 0; 
+      json.err = 0;
+      json.host = host;
       res.render('threshold.ejs', json);
     });
   }
 });
 
 app.get('/threshold', function(req, res){
-  var json = {};              
+  var json = {};
   switch(req.query['err']){
     case 1:
       json.err = 1;
@@ -98,6 +138,7 @@ app.get('/threshold', function(req, res){
   dbclient.getAllRooms(function(reply){
     json.rooms = reply;
     json.auth = 0;
+    json.host = host;
     res.render('threshold.ejs', json);
   });
 });
@@ -107,7 +148,7 @@ app.post('/room', function(req, res){
   if(body['nameInput'] && body['numberInput']){
     dbclient.createRoom(body['numberInput'], body['nameInput'], function(state){
       if(state){
-        dbclient.sendMessages(body['nameInput'], res);
+        res.redirect('/room?r=' + body['nameInput']);
       }
       else{
         res.redirect('/threshold?err=1');
@@ -116,14 +157,14 @@ app.post('/room', function(req, res){
   }
   else if(body['snameInput'] && body['snumberInput']){
     dbclient.subscribeRoom(body['snumberInput'], body['snameInput'], function(){
-      dbclient.sendMessages(body['snameInput'], res);
+      res.redirect('/room?r=' + body['snameInput']);
     });
   }
 });
 
 app.get('/room', function(req, res){
   if(req.query['r']){
-    dbclient.sendMessages(req.query['r'], res);
+    sendMessages(res, req.query['r']);
   }
   else{
     res.redirect('/threshold?err=2');
@@ -132,20 +173,21 @@ app.get('/room', function(req, res){
 
 app.get('/delete', function(req, res){
   if(!req.session.auth){
-    res.redirect('/threshold?err=3');;
+    res.redirect('/threshold?err=3');
   }
   else{
     dbclient.destroyRoom(req.query['r'], function(){
-      res.redirect('/admin');               
+      res.redirect('/admin');
     });
   }
 });
 
-/* TODO need to create a way of namespacing sockets so not emitting every message
+/* TODO need to create a way of namespacing sockets 
+ * so not emitting every message
  * TODO to every socket created, roooooooms*/
 app.get('/sms', function(req, res){
   if(req.query['clear'] == 1){
-    if(req.query['room'] == undefined){
+    if(!req.query['room']){
       res.send(400);
     }
     else{
@@ -160,34 +202,29 @@ app.get('/sms', function(req, res){
     res.send(204);
   }
   else{
-    var from = req.query['From'];  
+    var from = req.query['From'];
     from = from.substring(1, from.length);
     dbclient.forwardMessage(from, req.query['Body'], res, io);
   }
 });
 
-var readConfig = function(){
-  fs.readFile('./config.json', function(err, data){
-    if(err){
-      throw err;
-    }
-
-    var cdata = JSON.parse(data);
-    dbhost = cdata['dbhost'];
-    dbport = cdata['dbport'];
-    setLogging(cdata['log']);
-    authConnString = cdata['authConnString'];
-    connectDb();
-  });
-};
 
 var connectDb = function(){
   dbclient.connectDb(dbport, dbhost);
 };
 
 var setLogging = function(level){
-  if(level == 0){
+  if(level !== 0){
     io.set('log level', 1);
   }
 };
 
+var sendMessages = function(res, room){
+  dbclient.getMessages(room, function(msgs){
+    var json = {};
+    json.host = host;
+    json.room = room;
+    json.messTime = msgs;
+    res.render('room.ejs', json);
+  });
+};
